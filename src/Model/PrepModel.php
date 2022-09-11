391b096df4371d7200d41e2530356250961f1ba3
nw <a8gid24akL@gmail.com>
2022-09-11 03:18:44 -0400

    Create PrepModel.php

---
+++ src/Model/PrepModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace nathanwooten;
+
+class PrepModel
+{
+
+	public $modelName;
+	public $variables;
+
+	public function modelName( $modelName )
+	{
+
+		return $this->property( __FUNCTION__, $modelName );
+
+	}
+
+	public function variables( array $variables = null )
+	{
+
+		return $this->property( __FUNCTION__, $variables );
+
+	}
+
+	public function create()
+	{
+
+		return new {$this->get( 'modelName' )}( $this->variables );
+
+	}
+
+}
